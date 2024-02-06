644a26ce4d2ada38c00a0b975c2a0193306ddb5c
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-02-06 22:06:33 +0500

    fix(users): change staff role to worker role

--- database/migrations/20240206162310_users.ts
+++ database/migrations/20240206162310_users.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table) => {
         table.increments('id');
         table.string('name').notNullable();
-        table.enum('role', ['admin', 'director', 'staff']).notNullable();
+        table.enum('role', ['admin', 'director', 'worker']).notNullable();
         table
             .timestamp('created_at', { precision: 6, useTz: true })
             .defaultTo(knex.fn.now(6));
