fdfdcf30cc68a76910842826412918b80a5c4459
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-02-06 22:04:00 +0500

    feat(users): add table seed

---
+++ database/seeds/000_users.ts
@@ -0,0 +1,44 @@
+import { Knex } from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+    await knex('users').del();
+    await knex('users').insert([
+        {
+            // commenting ids allows them to be incremented naturally
+            // and helps to write seeds where user ids' are referenced
+            // id: 1,
+            name: 'Админ',
+            role: 'admin',
+        },
+        {
+            // id: 2,
+            name: 'Ахмед Малик',
+            role: 'director',
+        },
+        {
+            // id: 3,
+            name: 'Юсуф Кхан',
+            role: 'director',
+        },
+        {
+            // id: 4,
+            name: 'Леила Абиди',
+            role: 'staff',
+        },
+        {
+            // id: 5,
+            name: 'Али Ахмед',
+            role: 'staff',
+        },
+        {
+            // id: 6,
+            name: 'Амина Малик',
+            role: 'staff',
+        },
+        {
+            // id: 7,
+            name: 'Омар Салех',
+            role: 'staff',
+        },
+    ]);
+}
