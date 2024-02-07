5e9d052ac7e51414d4f32fbf3be65427dcb172f3
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-02-07 10:18:10 +0500

    fix(users): mimic organization-users as one-to-many

--- database/migrations/20240207041940_organization_users.ts
+++ database/migrations/20240207041940_organization_users.ts
@@ -4,11 +4,10 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('organization_users', (table) => {
         table.increments('id');
         table.integer('org_id').references('id').inTable('organizations');
-        table.integer('user_id').references('id').inTable('users');
         // It is said that this table should be created. But this table would link organizations and users
         // in a many-to-many relationship which I did not see the need for. Anyway, I created table but
-        // applied index below to turn relationship again to one-to-many manner
-        table.index(['org_id', 'user_id']);
+        // applied unique constraint to user_id column to turn relationship again to one-to-many manner
+        table.integer('user_id').references('id').inTable('users').unique();
     });
 }
 
