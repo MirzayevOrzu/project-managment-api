0521bf336c822f68f5e6594c937b64ee0e0e2f76
MirzayevOrzu <mirzayevorzu127@gmail.com>
2024-02-07 10:16:23 +0500

    fix(users): comment ids in organization users seed

--- database/seeds/020_organization_users.ts
+++ database/seeds/020_organization_users.ts
@@ -4,32 +4,32 @@ export async function seed(knex: Knex): Promise<void> {
     await knex('organization_users').del();
     await knex('organization_users').insert([
         {
-            id: 1,
+            // id: 1,
             org_id: 1,
             user_id: 2,
         },
         {
-            id: 2,
+            // id: 2,
             org_id: 2,
             user_id: 3,
         },
         {
-            id: 3,
+            // id: 3,
             org_id: 1,
             user_id: 4,
         },
         {
-            id: 4,
+            // id: 4,
             org_id: 1,
             user_id: 5,
         },
         {
-            id: 5,
+            // id: 5,
             org_id: 2,
             user_id: 6,
         },
         {
-            id: 6,
+            // id: 6,
             org_id: 2,
             user_id: 7,
         },
